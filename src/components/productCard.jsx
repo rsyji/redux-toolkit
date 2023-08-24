ec5164e9d19af5ef674137b43465123fd6f26e0d
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-08-24 14:36:15 +0530

    Update productCard.jsx
    static

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const ProductCard = () => {
+const ProductCard = ({image, title, price }) => {
+    
   return (
-    <div>ProductCard</div>
+    <div className='className="card w-32 h-60 text-center bg-slate-200 hover:bg-slate-400 rounded m-2'>
+        <img className='className="card-img-top w-[100%] h-[65%] self-center' src={image} alt=''/>
+        <div className=''>
+            <span className='font-bold'>$: {price}</span>
+            <h4 className='xl'>{title}</h4>
+        </div>
+        <button className='text-sm bg-yellow-200 hover:bg-yellow-300 rounded py-1 px-2'>Add to cart</button>
+    </div>
   )
 }
 
