dc85c3f2cf502f98cbf21f3ea1ce933bf450e6fe
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-08-24 11:47:24 +0530

    components and routes added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/home";
+import Cart from "./components/pages/cart";
+import NavBar from "./components/navBar";
 
 function App() {
   return (
-    <div className="App text-center">hello world
+    <div className="App">
+      <BrowserRouter>
+          <NavBar/>
+          <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/cart" element={<Cart/>}></Route>
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
