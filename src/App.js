3623dd684588f08e121ddb40c6d4448fc8cb3c9a
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-08-26 17:47:22 +0530

    Update App.js
    providing store access

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/home";
 import Cart from "./components/pages/cart";
 import NavBar from "./components/navBar";
+import { Provider } from "react-redux";
+import store from "./redux/store/store";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-          <NavBar/>
+      <Provider store={store}>
+        <BrowserRouter>
+          <NavBar />
           <Routes>
-            <Route path="/" element={<Home/>}></Route>
-            <Route path="/cart" element={<Cart/>}></Route>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
           </Routes>
-      </BrowserRouter>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
