d58834d71690794566d74bde5251d749d30e7b2e
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-08-26 17:44:08 +0530

    Update navBar.jsx
    using useSelector to changing state of cart

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const NavBar = () => {
+    const numberOfItems = useSelector(state=>state.cart)
   return (
     <div className='flex flex-row items-center bg-slate-500 text-white h-8'>
         <span className='font-extrabold basis-1/2 mx-2'> LOGO </span>
         <div className='basis-1/2 flex justify-evenly mx-2'> 
             <Link className='font-bold' to='/'>Home</Link>
             <Link className='font-bold' to='/cart'>Cart</Link>
-            <span> Cart items: 0 </span>
+            <span> Cart items: {numberOfItems.length} </span>
         </div>
     </div>
   )
