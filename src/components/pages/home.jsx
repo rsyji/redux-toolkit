6a134cb1c42b68930148aff056de5e5f3fd69905
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-08-26 17:46:34 +0530

    Update home.jsx

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,18 +1,7 @@
-import React ,{ useState, useEffect }from 'react'
+import React from 'react'
 import ProductCard from '../productCard'
 
 const Home = () => {
-    const [products, setProducts] = useState([])
-
-    useEffect(() => {
-      const fetchData= async()=>{
-        const data = await fetch('https://fakestoreapi.com/products')
-        const res = await data.json()
-        setProducts(res)
-
-    }
-    fetchData()
-    }, [])
 
   return (
     <div className='min-w-ful'>
@@ -20,11 +9,7 @@ const Home = () => {
             <h2 className='text-2xl font-bold'>Redux</h2>
         </div>
         <section className="container flex justify-center flex-wrap mx-2 px-2">
-            {
-                products? 
-                <>{products.map(product=> <ProductCard key={product.id} image={product.image} price={product.price} title={product.title.slice(0,12)} description={product.description.slice(0,20)} />)} </>
-                :"Loading..."
-            }
+            <ProductCard/>
         </section>
     </div>
   )
